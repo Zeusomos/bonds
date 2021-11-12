5d0f53ea6d9bace52ee73294d47b6606af8aea1e
Zeusomos <110340897+Zeusomos@users.noreply.github.com>
2021-11-12 13:26:32 +0000

    updated bundle

--- Rainfall/level0/source.c
+++ Rainfall/level0/source.c
@@ -8,3 +8,21 @@ int	main(int argc, char **argv)
 {
 	char	*exec_args[2];
 	gid_t	egid;
+	uid_t	euid;
+
+	if (atoi(argv[1]) == 423)
+	{
+		exec_args[0] = strdup("/bin/sh");
+		exec_args[1] = NULL;
+		egid = getegid();
+		euid = geteuid();
+		setresuid(euid, euid, euid);
+		setresgid(egid, egid, egid);
+		execv("/bin/sh", exec_args);
+	}
+	else
+	{
+		fwrite("No !\n", 1, 5, stderr);
+	}
+	return (0);
+}
---
+++ Rainfall/level0/walkthrough.md
@@ -0,0 +1,7 @@
+# Preparations
+
+Entering level0 home directory we see a level0 binary with sticky bit in access rights.
+
+# Code writing
+
+We
